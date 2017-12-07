6d5388dad64b3d7b505b89155b695a14073b6700
chotuaashiq <akshat.maheshwari@students.iiit.ac.in>
2017-12-07 16:36:55 +0530

    Templates and Queryset basics

--- Blog-With-Django-1.9/src/posts/views.py
+++ Blog-With-Django-1.9/src/posts/views.py
@@ -1,16 +1,42 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Post
 
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 def post_list(request):
-    return HttpResponse("<h1>List</h1>")
+    queryset = Post.objects.all()
+
+    context = {
+        "object_list" : queryset,
+        "title" : "List"
+    }
+
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title" : "My User List"
+    #     }
+    # else:
+    #     context = {
+    #         "title" : "List"
+    #     }
+
+    return render(request, "index.html", context)
+    # return HttpResponse("<h1>List</h1>")
 
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    # instance = Post.objects.get(10)
+    instance = get_object_or_404(Post, id=2)
+    context = {
+        "title" : instance.title,
+        "instance" : instance,
+    }
+
+    return render(request, "post_detail.html", context)
+    # return HttpResponse("<h1>Detail</h1>")
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
