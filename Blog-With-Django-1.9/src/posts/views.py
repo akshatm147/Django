fafca252a8f4203f0676c6e7cc0e851509f1f1fe
chotuaashiq <akshat.maheshwari@students.iiit.ac.in>
2017-12-17 15:49:11 +0530

    Basic User permissions added, Only Admin or staff can create, update and delete posts

--- Blog-With-Django-1.9/src/posts/views.py
+++ Blog-With-Django-1.9/src/posts/views.py
@@ -1,6 +1,6 @@
 from urllib.parse import quote_plus
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
@@ -9,6 +9,9 @@ from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
     form = PostForm(request.POST or None, request.FILES or None)
 
     if form.is_valid():
@@ -78,6 +81,9 @@ def post_detail(request, slug=None):
     # return HttpResponse("<h1>Detail</h1>")
 
 def post_update(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
     instance = get_object_or_404(Post, slug=slug)
 
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
@@ -96,6 +102,9 @@ def post_update(request, id=None):
     return render(request, "post_update.html", context)
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Post Deleted Successfully")
